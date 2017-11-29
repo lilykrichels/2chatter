b992a53ff0ce2ed50a5b3a6632125d038086a146
Lily Krichels <lily.krichels@gmail.com>
2017-11-28 20:32:44 -0500

    Added Edmx and pulled in database

---
+++ 2_Chatter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_2_Chatter.Startup))]
+namespace _2_Chatter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
